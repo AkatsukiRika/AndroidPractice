c36e03bcba67b79979b13eb076d8cb9613988c19
AkatsukiRika <m1345860061@gmail.com>
2023-06-02 11:06:06 +0800

    修复json文件列表无法滑动到底部的bug

--- app/src/main/java/com/tangping/androidpractice/ui/memorize/create/CreateMemoryCardsScreen.kt
+++ app/src/main/java/com/tangping/androidpractice/ui/memorize/create/CreateMemoryCardsScreen.kt
@@ -1,8 +1,6 @@
 package com.tangping.androidpractice.ui.memorize.create
 
 import android.util.Log
-import android.view.MotionEvent
-import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -14,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.sharp.Close
 import androidx.compose.material.icons.sharp.Done
@@ -29,18 +26,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.tangping.androidpractice.R
 import com.tangping.androidpractice.ui.theme.darkBackground
@@ -83,18 +78,22 @@ fun CreateMemoryCardsScreen(
             }
         )
 
-        DoneButton(
-            modifier = Modifier.constrainAs(btnDone) {
-                top.linkTo(parent.top)
-                end.linkTo(parent.end)
-            }
-        )
+        if (!showFileSelector) {
+            DoneButton(
+                modifier = Modifier.constrainAs(btnDone) {
+                    top.linkTo(parent.top)
+                    end.linkTo(parent.end)
+                }
+            )
+        }
 
         if (showFileSelector) {
             FileSelector(
                 modifier = Modifier.constrainAs(fileSelector) {
-                    top.linkTo(btnDone.bottom)
+                    top.linkTo(btnClose.bottom)
+                    bottom.linkTo(parent.bottom)
                     start.linkTo(parent.start)
+                    height = Dimension.fillToConstraints
                 },
                 onInit = {
                     viewModel.dispatch(CreateMemoryCardsAction.ScanCacheDirectory, context)
